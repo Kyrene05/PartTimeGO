78e3c1303b9bb2b04a395dcc8aee94c7c67b3dc0
zhaomei1219 <zhaomei1219@gmail.com>
2026-08-31 11:17:08 +0800

    add page （still not complete)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
     defaultConfig {
         applicationId = "com.example.parttimego"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/com/example/parttimego/screen/EmployerProfileScreen.kt
@@ -0,0 +1,6 @@
+package com.example.parttimego.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EmployerProfileScreen(){}
